fd132b0cd588744e050736f13a256f6ab18ac985
Shauni Doms <doms@evolbio.mpg.de>
2023-10-10 14:20:41 +0200

    First commit

---
+++ PurrfectPlots_script.R
@@ -0,0 +1,7 @@
+setwd("~/Documents/Research/Scripts/PurrfectPlots")
+
+devtools::create("PurrfectPlots")
+
+lily<- c("#cfc9bd","#29231e","#9e8772","#b0b0a6","#7e796e","#765f4c","#5c5953","#a3a3a4","#6d6b6c","#bc7c6c")
+binky <- c("#805C5C", "#3A291B","#374D4D", "#496749")
+teddy <- c("")
